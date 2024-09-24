fd3fa5f944805e3957609bf8712ef00268d246e1
Gustavo Bertuzzi <gustavobertuzziif@gmail.com>
2024-09-24 19:12:01 -0300

    Formatando

--- aed/libs/entrada.java
+++ aed/libs/entrada.java
@@ -5,53 +5,65 @@ import java.util.Scanner;
 public class entrada {
     public static Scanner entrada;
 
-    public static void abrir(){
+    public static void abrir() {
         entrada = new Scanner(System.in);
     }
-    public static void fechar(){
+
+    public static void fechar() {
         entrada.close();
     }
-    public static int lerInt(String msg){
+
+    public static int lerInt(String msg) {
         System.out.println(msg);
         return entrada.nextInt();
     }
-     public static int lerInt(){
+
+    public static int lerInt() {
         return entrada.nextInt();
     }
-    public static double lerDouble(){
+
+    public static double lerDouble() {
         return entrada.nextDouble();
     }
-    public static double lerDouble(String msg){
+
+    public static double lerDouble(String msg) {
         System.out.println(msg);
         return entrada.nextInt();
     }
-    public static String lerPalavra(){
+
+    public static String lerPalavra() {
         return entrada.next();
     }
-    public static String lerPalavra(String msg){
+
+    public static String lerPalavra(String msg) {
         System.out.println(msg);
         return entrada.next();
     }
-    public static char lerCaractere(String msg){
+
+    public static char lerCaractere(String msg) {
         System.out.println(msg);
         return lerPalavra().charAt(0);
     }
-    public static char lerCaractere(){
+
+    public static char lerCaractere() {
         return lerPalavra().charAt(0);
     }
-    public static String lerFrase(){
+
+    public static String lerFrase() {
         return entrada.nextLine();
     }
-    public static String lerFrase(String msg){
+
+    public static String lerFrase(String msg) {
         System.out.println(msg);
         return entrada.nextLine();
     }
-    public static boolean lerBoolean(String msg){
+
+    public static boolean lerBoolean(String msg) {
         System.out.println(msg);
         return entrada.nextBoolean();
     }
-    public static boolean lerBoolean(){
+
+    public static boolean lerBoolean() {
         return entrada.nextBoolean();
     }
 }
-
